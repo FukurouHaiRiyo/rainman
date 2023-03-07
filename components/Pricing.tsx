b8029d9241721d9e041fbadba152534fe7284588
FukurouHaiRiyo <andreipanait00@gmail.com>
2023-03-07 17:12:12 +0200

    Update Pricing.tsx

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -14,7 +14,6 @@ import {
 } from '@chakra-ui/react';
 import { FaCheckCircle } from 'react-icons/fa';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import {Team} from './Team';
 
 function PriceWrapper({ children }: { children: ReactNode }) {
   return (
