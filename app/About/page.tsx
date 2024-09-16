5673d766eb38b677e9ea1161a70195685acbb813
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-09-16 10:51:52 +0300

    updates

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -4,7 +4,7 @@ import React, {useState} from 'react';
 
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import Navbar from '../../components/navbar';
+import Navbar from '@/components/Navbar';
 import ContactUs from '../Contact/page';
 
 const AboutUs = () => {
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import Navbar from '@/components/navbar';
+import Navbar from '@/components/Navbar';
 import ContactUs from './Contact/page';
 
 const navigation = [
