d9d23f1c9fa4e47b6cc082a0aabca525676d6101
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-02 23:59:02 +0300

    updates

--- components/Common/SectionTitle.tsx
+++ components/Common/SectionTitle.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
 const SectionTitle = ({
-  title, paragraph, width = '570px', center, mb = '100px'
+  title,
+  paragraph,
+  width = '570px',
+  center,
+  mb = '100px',
 }: {
-  title: string, paragraph: string, width?: string, center?: boolean, mb?: string;
+  title: string;
+  paragraph: string;
+  width?: string;
+  center?: boolean;
+  mb?: string;
 }) => {
   return (
-    <div className={`w-full ${center ? 'mx-auto text-center': ''}`} style={{ maxWidth: width, marginBottom: mb}}>
-      <h2 className='mb-4 text-3xl font-bold text-foreground sm:text-4xl'>
-          {title}
+    <div
+      className={`w-full ${center ? 'mx-auto text-center' : ''}`}
+      style={{ maxWidth: width, marginBottom: mb }}
+    >
+      <h2 className="mb-4 text-3xl font-bold text-foreground sm:text-4xl">
+        {title}
       </h2>
-      <p className='text-muted-foreground'>
-        {paragraph}
-       </p>
+      <p className="text-muted-foreground">{paragraph}</p>
     </div>
-  )
-}
+  );
+};
 
 export default SectionTitle;
