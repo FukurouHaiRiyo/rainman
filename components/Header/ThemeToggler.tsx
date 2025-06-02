bafcff5cce35cc63e51f27b63a3f6dc45b2deb43
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-02 21:04:48 +0300

    Update ThemeToggler.tsx

--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -6,7 +6,7 @@ const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
   return (
     <button aria-label='theme toggler'
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'light')}
+      onClick={() => setTheme(theme === 'dark' ? 'dark' : 'light')}
       className='flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14'
     >
       <svg
