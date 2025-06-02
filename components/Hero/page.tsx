792722b1e95a547943d02cede4c7c3bb8c439d46
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-02 22:19:57 +0300

    Update page.tsx

--- components/Hero/page.tsx
+++ components/Hero/page.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <>
       <section
         id='home'
-        className='relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]'
+        className='relative z-10 overflow-hidden pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]'
       >
         <div className='container'>
           <div className='-mx-4 flex flex-wrap'>
@@ -23,16 +23,16 @@ const Hero = () => {
                 </p>
                 <div className='flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0'>
                   <Link
-                    href='https://nextjstemplates.com/templates/saas-starter-startup'
+                    href='/'
                     className='rounded-xs bg-primary px-8 py-4 text-base font-semibold text-black duration-300 ease-in-out hover:bg-primary/80'
                   >
-                    🔥 Get Pro
+                    Afla mai multe 
                   </Link>
                   <Link
-                    href='https://github.com/NextJSTemplates/startup-nextjs'
+                    href='/'
                     className='inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5'
                   >
-                    Star on GitHub
+                    Contacteaza-ne
                   </Link>
                 </div>
               </div>
