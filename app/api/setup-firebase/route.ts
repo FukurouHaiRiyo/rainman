2c09b02543b20ff008edbcf24f5f7aa17c454886
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-02 10:46:50 +0300

    Update route.ts

--- app/api/setup-firebase/route.ts
+++ app/api/setup-firebase/route.ts
@@ -5,9 +5,9 @@ import { getDatabase } from 'firebase-admin/database';
 if (!getApps().length) {
     initializeApp({
       credential: cert({
-        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     })
