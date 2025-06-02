fa2cbae4eaa4876ab73c512d2ee0383b98b61884
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-02 21:19:44 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
-import "./globals.css"
 import { auth } from "@clerk/nextjs/server"
 import { Toaster } from "@/components/ui/toaster"
 
