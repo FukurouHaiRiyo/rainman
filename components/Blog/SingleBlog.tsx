70f8271cc37379964655669d681284d8b76eae8c
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-03 20:41:15 +0300

    Update SingleBlog.tsx

--- components/Blog/SingleBlog.tsx
+++ components/Blog/SingleBlog.tsx
@@ -1,18 +1,24 @@
-// components/Blog/SingleBlog.tsx
-import Link from 'next/link';
 import { Blog } from '@/types/blog';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const SingleBlog = ({ blog }: { blog: Blog }) => {
+  const { slug, title, image, paragraph, author, tags, publishDate } = blog;
+
   return (
-    <div className='rounded-lg shadow-lg overflow-hidden bg-white dark:bg-dark p-4'>
-      <Link href={`/blog/${blog.slug}`}>
-        <div className='relative w-full h-56 mb-4'>
-          <Image src={blog.image} alt={blog.title} fill className='object-cover rounded-md' />
-        </div>
-        <h3 className='text-xl font-bold mb-2 text-black dark:text-white'>{blog.title}</h3>
-        <p className='text-body-color'>{blog.paragraph}</p>
+    <div className='group shadow-one hover:shadow-two dark:bg-card bg-white rounded-xs overflow-hidden'>
+      <Link href={`/blog/${slug}`} className='block relative aspect-[16/9]'>
+        <Image src={image} alt={title} fill className='object-cover' />
       </Link>
+      <div className='p-6'>
+        <h3>
+          <Link href={`/blog/${slug}`} className='text-xl font-bold hover:text-primary'>
+            {title}
+          </Link>
+        </h3>
+        <p className='mt-4 text-muted-foreground'>{paragraph}</p>
+        <div className='mt-4 text-sm text-body-color'>By {author.name} · {publishDate}</div>
+      </div>
     </div>
   );
 };
