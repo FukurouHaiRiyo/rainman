732dd05fe87b4324d23fa58c328649574eda4a76
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-03 21:11:13 +0300

    Update page.tsx

--- components/Hero/page.tsx
+++ components/Hero/page.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
                     </button>
                   </Link>
                   <Link
-                    href='/'
+                    href='/Contact'
                     className='inline-block rounded-xs bg-muted px-8 py-4 text-base font-semibold text-muted-foreground hover:bg-muted/80'
                   >
                     <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
