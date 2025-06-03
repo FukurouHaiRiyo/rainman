c6c8a8e5e927616a2dc8b30a3c4697656de4c0cc
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2025-06-03 21:36:27 +0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import { auth } from "@clerk/nextjs/server"
-import { useUserRole } from "@/context/user-context"
 import { redirect } from "next/navigation"
 import { Sidebar1 } from "@/components/dashboard/sidebar"
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const { userId } = await auth()
-  const { role, isLoading } = useUserRole()
 
   // If not signed in, redirect to sign-in page
   if (!userId) {
@@ -15,7 +13,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
 
   return (
     <div className="flex h-screen">
-      {role !== 'guest' ? (<Sidebar1 />): (null)}
+      <Sidebar1 />
       <main className="flex-1 overflow-auto">{children}</main>
     </div>
   )
